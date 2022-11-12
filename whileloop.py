3a07c4f3308c35e39e051c74990123c014a64d1c
DeepakRanaMagar <dpk.me.123@gmail.com>
2022-11-13 04:03:27 +0545

    week 1

---
+++ whileloop.py
@@ -0,0 +1,12 @@
+#loops: while, for
+x = int(input('Enter any number to compare: '))
+
+while x<10:
+    print('The given number is less than 10')
+    break
+else:
+    if x ==10:
+        print('The given number is equal to 10')
+    else:
+        print('The given number is more than 10')
+
