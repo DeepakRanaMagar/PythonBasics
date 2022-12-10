95dd5fad88ef8bd02fb0ca0d4adafb4548080735
DeepakRanaMagar <dpk.me.123@gmail.com>
2022-12-10 13:47:31 +0545

    Library-commit

---
+++ libraries/numPhy1.py
@@ -0,0 +1,21 @@
+# numPhy -> Numerical python 
+# library used for working with array 
+# contains functions for working in domain of linear algebra, foureir transform, and matrices.
+# 
+# provides an array object with in faster than the python list 
+# "ndarray" is the array object 
+# stores at one continuous place in memory unlike lists.
+# 
+# alias np  
+
+
+# converting list and tuple into ndarray
+import numpy as np
+
+arr = np.array((1,2,3,4,5))
+arr1 = np.array([1,2,3,4,5])
+
+print(type(arr))
+
+print(type(arr1))
+ 
