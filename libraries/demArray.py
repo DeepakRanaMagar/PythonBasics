bee659671531d065a68f7803c55723c86fcc5acb
DeepakRanaMagar <dpk.me.123@gmail.com>
2022-12-10 13:59:38 +0545

    Library-commit

---
+++ libraries/demArray.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# scalar 0-D array
+arr = np.array(23)
+print(arr)
+
+# 2D array
+arr1 = np.array([[1,2],[2,3]])
+print(arr1)
+
+# 3D array
+
+arr2 = np.array([[1,2,3],[3,4,5],[5,6,7]])
+print(arr2.ndim)
+
+
