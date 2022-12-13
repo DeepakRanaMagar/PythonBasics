89fab8b09985995e37db56dc579d900787bcd7ce
DeepakRanaMagar <dpk.me.123@gmail.com>
2022-12-13 06:06:58 +0545

    DataFrame

---
+++ libraries/dataframe.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+
+# data = {
+#     "subject": ['ADA', 'AOS', 'AINN'],
+#     "hours": [10, 20, 30]
+# }
+# var = pd.DataFrame(data, index=["sunday","monday", "tuesday"])
+# print(var)
+
+#     # locate
+# print(var.loc["monday"])
+
+    # load files into data frames
+df = pd.read_csv('data.cvs')
+print(df)
